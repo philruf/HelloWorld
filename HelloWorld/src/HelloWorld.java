eeae5f47c500ed9a913a7bd2a5bd32387ddf5d12
Ruf.Philipp <Ruf.Philipp@SK-D43D7EB3BAE9.2631.skool.net>
2017-09-20 16:54:29 +0200

    Collatz Folge fertig

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -9,14 +9,14 @@ public class HelloWorld {
 	System.out.print(n + ": ");
 	
 	
-	while(n>1){
-	if (n%2 == 0){
-		n = n/2;
-	}
-	else{
-		n = n * 3 +1;
-	}
-	
+		while(n>1){
+			if (n%2 == 0){
+				n = n/2;
+				  }
+			else{
+				n = n * 3 +1;
+				}
+				
 	System.out.print(n + " ");
 	}
 	System.out.println("");
