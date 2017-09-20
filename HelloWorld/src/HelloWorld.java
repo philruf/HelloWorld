88a102d1c6e9d6cff50bc6366a797c2bdbb44474
Ruf.Philipp <Ruf.Philipp@SK-D43D7EB3BAE9.2631.skool.net>
2017-09-20 16:54:09 +0200

    Collatz Versuch nicht gelöst...

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,7 +2,14 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		int n = 19;
+		int n;
+	
+	for(int i=1; i<=300; i++){
+		n=i;
+	System.out.print(n + ": ");
+	
+	
+	while(n>1){
 	if (n%2 == 0){
 		n = n/2;
 	}
@@ -10,7 +17,10 @@ public class HelloWorld {
 		n = n * 3 +1;
 	}
 	
-	System.out.println(n);
+	System.out.print(n + " ");
+	}
+	System.out.println("");
+	}
 	}
 }
 	/* static = code darf sich bei laufzeit nicht verändern*/
