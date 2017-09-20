714a92bac6704931b7c618a360685dc94fc8c196
Ruf.Philipp <Ruf.Philipp@SK-D43D7EB3BAE9.2631.skool.net>
2017-09-20 16:28:34 +0200

    Collatz Versuch

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,10 +2,19 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("HELLOWORLD");
+		int n = 19;
+	if (n%2 == 0){
+		n = n/2;
+	}
+	else{
+		n = n * 3 +1;
 	}
 	
-
-
-
+	System.out.println(n);
+	}
 }
+	/* static = code darf sich bei laufzeit nicht verändern*/
+
+
+
+
